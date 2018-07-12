c6d716a911c9f4cff23dc4d5118eaffe2b15fc5e
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2018-07-12 18:03:10 -0400

    Remove debug message

--- src/app/modules/registration-status/components/registration-status.component.ts
+++ src/app/modules/registration-status/components/registration-status.component.ts
@@ -51,7 +51,6 @@ export class RegistrationStatusComponent extends Base implements OnInit {
    * @returns {boolean}
    */
   canContinue(): boolean {
-    console.log( 'Form: ', this.form );
     return this.form.valid;
   }
 
