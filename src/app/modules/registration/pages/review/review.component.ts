4df39c72981dc0a868d8be853bff211081b71b3b
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2018-07-24 18:30:59 -0400

    Checkpoint - review page

--- src/app/modules/registration/pages/review/review.component.ts
+++ src/app/modules/registration/pages/review/review.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, DoCheck, OnInit} from '@angular/core';
+import {AbstractFormComponent} from '../../../../models/abstract-form-component';
+import {FPCareDataService} from '../../../../services/fpcare-data.service';
+import {DateTimeService} from '../../../../services/date-time.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'fpcare-review',
   templateUrl: './review.component.html',
   styleUrls: ['./review.component.scss']
 })
-export class ReviewPageComponent implements OnInit {
+export class ReviewPageComponent extends AbstractFormComponent implements OnInit, DoCheck {
 
-  constructor() { }
+  constructor( private fpcService: FPCareDataService
+    , private dateTimeService: DateTimeService
+    , protected router: Router ) {
+    super( router );
+  }
 
   ngOnInit() {
   }
 
+  /**
+   * Detect changes, check if form is valid
+   */
+  ngDoCheck() { }
+
+  // Methods triggered by the form action bar
+
+  /**
+   * Navigates to the next page
+   */
+  continue() {
+
+    if ( this.canContinue() ) {
+      const link = '/registration/child-info';
+      this.router.navigate([link]);
+    }
+  }
 }
