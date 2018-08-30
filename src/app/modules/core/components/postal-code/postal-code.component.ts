8fe8548b8c8da30a19ae69d1b066d6f34a5716d3
Adam Coard <arcoard+github@gmail.com>
2018-08-30 13:08:45 -0400

    PostalCodeComponent now automatically populates parents ngForm object

--- src/app/modules/core/components/postal-code/postal-code.component.ts
+++ src/app/modules/core/components/postal-code/postal-code.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Base } from '../base/base.class';
+import { ControlContainer, NgForm } from '@angular/forms';
 
 const LETTER = /[A-Z]/i; //Ignore case here, then upperCase it via pipe.
 const NUMBER = /\d/;
@@ -7,7 +8,11 @@ const NUMBER = /\d/;
 @Component({
   selector: 'fpcare-postal-code',
   templateUrl: './postal-code.component.html',
-  styleUrls: ['./postal-code.component.scss']
+  styleUrls: ['./postal-code.component.scss'],
+
+  // Re-use the same ngForm that it's parent is using. The component will show
+  // up in its parents `this.form`, and will auto-update `this.form.valid`
+  viewProviders: [ { provide: ControlContainer, useExisting: NgForm }]
 })
 export class PostalCodeComponent extends Base implements OnInit {
   @Input() value: string;
