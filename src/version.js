6942eb56766785b76678dfb2f916ca035edc7980
Adam Coard <arcoard+github@gmail.com>
2018-10-11 14:59:19 -0400

    Update commits link

--- src/version.js
+++ src/version.js
@@ -34,7 +34,7 @@ export const buildTime = '${ buildTime }';
 /** App version retrieved from package.json. */
 export const projectVersion = '${projectVersion}';
 /** Human readable message  */
-export const message = 'Canonical Version: ${ trimmed } - ${ buildTime } (v${projectVersion}) \\nhttp://github.com/bcgov/moh-fpcare/commit/${ trimmed }';
+export const message = 'Canonical Version: ${ trimmed } - ${ buildTime } (v${projectVersion}) \\nhttp://github.com/bcgov/moh-fpcare/commits/${ trimmed }';
 /** If true, other values should be present. */
 export const success = ${success};
 `
