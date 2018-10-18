82456562b76b1bcc5a6c05e9b383a99aac3333e0
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2018-10-18 13:05:19 -0400

    Fixed merge issue with tests

--- src/app/modules/registration/pages/reg-results/reg-results.component.spec.ts
+++ src/app/modules/registration/pages/reg-results/reg-results.component.spec.ts
@@ -5,11 +5,8 @@ import {CoreModule} from '../../../core/core.module';
 import {RegistrationService} from '../../registration.service';
 import {RouterTestingModule} from '@angular/router/testing';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
-<<<<<<< HEAD
-=======
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FinancialCalculatorModule} from '../../../financial-calculator/financial-calculator.module';
->>>>>>> registration
 
 describe('RegResultsComponent', () => {
   let component: RegResultsComponent;
@@ -23,13 +20,9 @@ describe('RegResultsComponent', () => {
       imports: [
         CoreModule,
         HttpClientTestingModule,
-<<<<<<< HEAD
-        RouterTestingModule
-=======
         RouterTestingModule,
         FinancialCalculatorModule,
         BrowserAnimationsModule
->>>>>>> registration
       ],
       providers: [
         RegistrationService
